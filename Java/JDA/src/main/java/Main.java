94fa36634f6a579bd15d10746d6a1a016ecbfeda
xhayper <ikq31421@gmail.com>
2020-08-09 06:36:47 +0700

    Update Main.java

--- Java/JDA/src/main/java/Main.java
+++ Java/JDA/src/main/java/Main.java
@@ -3,7 +3,6 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
 import javax.security.auth.login.LoginException;
 
 public class Main extends ListenerAdapter {
