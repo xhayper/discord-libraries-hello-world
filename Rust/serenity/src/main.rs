3a63891042b45ae9cd9eec33eeb72d4ff5112523
xhayper <ikq31421@gmail.com>
2020-08-09 06:28:41 +0700

    Update main.rs

--- Rust/serenity/src/main.rs
+++ Rust/serenity/src/main.rs
@@ -10,14 +10,14 @@ struct Handler;
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "hello" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Hello world! Rust") {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Hello, world! Rust") {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
     fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+        println!("Logged in as {}", ready.user.tag);
     }
 }
 
@@ -25,7 +25,7 @@ fn main() {
     let token = env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment");
 
-    let mut client = Client::new(&token, Handler).expect("Err creating client");
+    let mut client = Client::new(&token, Handler).expect("Error creating client");
 
     if let Err(why) = client.start() {
         println!("Client error: {:?}", why);
